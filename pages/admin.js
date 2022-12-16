5041e8480bcf2d064f4d1c3419d927534222529a
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2022-12-16 19:03:50 +0530

    fix

--- pages/admin.js
+++ pages/admin.js
@@ -33,7 +33,9 @@ const { decodedToken, isExpired ,reEvaluateToken} = useJwt(token);
   position="top-center"
   reverseOrder={true}
 
-/>
+/><Link href="/" className="bg-gray-100 flex flex-row items-center justify-center rounded-full w-10 h-10 hover:shadow-xl fixed top-5 left-5">
+        <svg className="w-7 h-7 fill-lime-700" clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m9.474 5.209s-4.501 4.505-6.254 6.259c-.147.146-.22.338-.22.53s.073.384.22.53c1.752 1.754 6.252 6.257 6.252 6.257.145.145.336.217.527.217.191-.001.383-.074.53-.221.293-.293.294-.766.004-1.057l-4.976-4.976h14.692c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-14.692l4.978-4.979c.289-.289.287-.761-.006-1.054-.147-.147-.339-.221-.53-.221-.191-.001-.38.071-.525.215z" fillRule="nonzero"/></svg>
+      </Link>
       { loginStatus==0 && <PleaseLogin></PleaseLogin>}
       { loginStatus==1 && <PleaseLogin m={"Login Expired"}></PleaseLogin>}
       { loginStatus==2 && <AdminP contacts={contacts} response={response} email={x.email}></AdminP>}
@@ -44,9 +46,7 @@ function PleaseLogin({m}){
 
   return(
     <div className="flex flex-col items-center text-center justify-center h-full w-full">
-     <Link href="/" className="bg-gray-100 flex flex-row items-center justify-center rounded-full w-10 h-10 hover:shadow-xl fixed top-5 left-5">
-        <svg className="w-7 h-7 fill-lime-700" clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m9.474 5.209s-4.501 4.505-6.254 6.259c-.147.146-.22.338-.22.53s.073.384.22.53c1.752 1.754 6.252 6.257 6.252 6.257.145.145.336.217.527.217.191-.001.383-.074.53-.221.293-.293.294-.766.004-1.057l-4.976-4.976h14.692c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-14.692l4.978-4.979c.289-.289.287-.761-.006-1.054-.147-.147-.339-.221-.53-.221-.191-.001-.38.071-.525.215z" fillRule="nonzero"/></svg>
-      </Link>
+     
       {m ? m : "No Loing Information Found !"}<br></br>
       Kindly Login First<br></br>
       <Link href="/" className="bg-black text-white rounded p-1 m-1 tracking-wider">Home</Link>
